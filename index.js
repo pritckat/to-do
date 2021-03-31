8a9e6485b3ad9020341f4e362c92eee6c5d2c0f5
pritckat <pritckat@gmail.com>
2021-03-31 12:53:25 -0400

    create gitignore

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const MongoClient = require('mongodb')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
